d68a3e4741351e2bcd375678854dd844758e9c2c
palascinochristian <palascinochristian@gmail.com>
2024-11-27 13:10:45 +0100

    strings > second exercise done

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -10,6 +10,13 @@ console.log(userName);
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+let number = 24;
+let text = "Ventiquattro";
+let bool = false;
+
+let finalPrint = number + " " + text + " " + bool;
+console.log(finalPrint)
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
